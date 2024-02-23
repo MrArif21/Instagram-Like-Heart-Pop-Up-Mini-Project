5daaee91baad32bb296349ee7050912fa26ed2a8
MrArif21 <mr.mohdarif21@gmail.com>
2024-02-23 17:40:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let icon = document.querySelector("#container")
+let love = document.querySelector("i")
+
+icon.addEventListener("dblclick",function (){
+    love.style.transform = 'translate(-50%,-50%) scale(1)';
+    love.style.opacity = 0.8
+
+    setTimeout(function(){
+        love.style.opacity = 0
+    },1000);
+
+    setTimeout(function(){
+        love.style.transform = 'translate(-50%,-50%) scale(0)';
+        },2000);
+})
+
